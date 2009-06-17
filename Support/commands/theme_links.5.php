dbfb7dfc941ad93fad94ff19780dbbc98f8220b3
Richard Burford <rich@freestylesystems.co.uk>
2009-06-17 21:46:14 +0000

    Adding theme_links snippets.
    git-svn-id: http://svn.textmate.org/trunk/Review/Bundles/PHP Drupal.tmbundle@11642 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

---
+++ Support/commands/theme_links.5.php
@@ -0,0 +1,71 @@
+/**
+ * Return a themed set of links.
+ *
+ * @param \$links
+ *   A keyed array of links to be themed.
+ * @param \$attributes
+ *   A keyed array of attributes
+ * @return
+ *   A string containing an unordered list of links.
+ */
+function theme_links(\$links, \$attributes = array('class' => 'links')) {
+  \$output = '';
+
+  if (count(\$links) > 0) {
+    \$output = '<ul'. drupal_attributes(\$attributes) .'>';
+
+    \$num_links = count(\$links);
+    \$i = 1;
+
+    foreach (\$links as \$key => \$link) {
+      \$class = '';
+
+      // Automatically add a class to each link and also to each LI
+      if (isset(\$link['attributes']) && isset(\$link['attributes']['class'])) {
+        \$link['attributes']['class'] .= ' ' . \$key;
+        \$class = \$key;
+      }
+      else {
+        \$link['attributes']['class'] = \$key;
+        \$class = \$key;
+      }
+
+      // Add first and last classes to the list of links to help out themers.
+      \$extra_class = '';
+      if (\$i == 1) {
+        \$extra_class .= 'first ';
+      }
+      if (\$i == \$num_links) {
+        \$extra_class .= 'last ';
+      }
+      \$output .= '<li class="'. \$extra_class . \$class .'">';
+
+      // Is the title HTML?
+      \$html = isset(\$link['html']) && \$link['html'];
+
+      // Initialize fragment and query variables.
+      \$link['query'] = isset(\$link['query']) ? \$link['query'] : NULL;
+      \$link['fragment'] = isset(\$link['fragment']) ? \$link['fragment'] : NULL;
+
+      if (isset(\$link['href'])) {
+        \$output .= l(\$link['title'], \$link['href'], \$link['attributes'], \$link['query'], \$link['fragment'], FALSE, \$html);
+      }
+      else if (\$link['title']) {
+        //Some links are actually not links, but we wrap these in <span> for adding title and class attributes
+        if (!\$html) {
+          \$link['title'] = check_plain(\$link['title']);
+        }
+        \$output .= '<span'. drupal_attributes(\$link['attributes']) .'>'. \$link['title'] .'</span>';
+      }
+
+      \$i++;
+      \$output .= "</li>\n";
+    }
+
+    \$output .= '</ul>';
+  }
+
+  return \$output;
+}
+
+$1
---
+++ Support/commands/theme_links.6.php
@@ -0,0 +1,63 @@
+/**
+ * Return a themed set of links.
+ *
+ * @param \$links
+ *   A keyed array of links to be themed.
+ * @param \$attributes
+ *   A keyed array of attributes
+ * @return
+ *   A string containing an unordered list of links.
+ */
+function theme_links(\$links, \$attributes = array('class' => 'links')) {
+  global \$language;
+  \$output = '';
+
+  if (count(\$links) > 0) {
+    \$output = '<ul'. drupal_attributes(\$attributes) .'>';
+
+    \$num_links = count(\$links);
+    \$i = 1;
+
+    foreach (\$links as \$key => \$link) {
+      \$class = \$key;
+
+      // Add first, last and active classes to the list of links to help out themers.
+      if (\$i == 1) {
+        \$class .= ' first';
+      }
+      if (\$i == \$num_links) {
+        \$class .= ' last';
+      }
+      if (isset(\$link['href']) && (\$link['href'] == \$_GET['q'] || (\$link['href'] == '<front>' && drupal_is_front_page()))
+          && (empty(\$link['language']) || \$link['language']->language == \$language->language)) {
+        \$class .= ' active';
+      }
+      \$output .= '<li'. drupal_attributes(array('class' => \$class)) .'>';
+
+      if (isset(\$link['href'])) {
+        // Pass in \$link as \$options, they share the same keys.
+        \$output .= l(\$link['title'], \$link['href'], \$link);
+      }
+      else if (!empty(\$link['title'])) {
+        // Some links are actually not links, but we wrap these in <span> for adding title and class attributes
+        if (empty(\$link['html'])) {
+          \$link['title'] = check_plain(\$link['title']);
+        }
+        \$span_attributes = '';
+        if (isset(\$link['attributes'])) {
+          \$span_attributes = drupal_attributes(\$link['attributes']);
+        }
+        \$output .= '<span'. \$span_attributes .'>'. \$link['title'] .'</span>';
+      }
+
+      \$i++;
+      \$output .= "</li>\n";
+    }
+
+    \$output .= '</ul>';
+  }
+
+  return \$output;
+}
+
+$1
