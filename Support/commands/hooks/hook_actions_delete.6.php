ad2629e65185d4dcf6d9b59214c60d01f617ff50
Richard Burford <rich@freestylesystems.co.uk>
2009-06-19 13:11:59 +0000

    Adding more snippet creation functions.
    git-svn-id: http://svn.textmate.org/trunk/Review/Bundles/PHP Drupal.tmbundle@11661 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

---
+++ Support/commands/hooks/hook_actions_delete.6.php
@@ -0,0 +1,8 @@
+/**
+ * Implementation of hook_actions_delete().
+ */
+function <?php print $basename; ?>_actions_delete(\$aid) {${1:
+  db_query("DELETE FROM {actions_assignments\} WHERE aid = '%s'", \$aid);}
+}
+
+$2
