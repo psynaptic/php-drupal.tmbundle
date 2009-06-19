b9f5f9d57b14f9e97bf6f8131c1ba81ce0470b0f
Richard Burford <rich@freestylesystems.co.uk>
2009-06-19 15:07:03 +0000

    Added core action module hooks.
    git-svn-id: http://svn.textmate.org/trunk/Review/Bundles/PHP Drupal.tmbundle@11665 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Support/commands/hooks/hook_actions_delete.6.php
+++ Support/commands/hooks/hook_actions_delete.6.php
@@ -2,7 +2,7 @@
  * Implementation of hook_actions_delete().
  */
 function <?php print $basename; ?>_actions_delete(\$aid) {${1:
-  db_query("DELETE FROM {actions_assignments\} WHERE aid = '%s'", \$aid);}
+  db_query("DELETE FROM {${2:actions_assignments}\} WHERE aid = '%s'", \$aid);}
 }
 
-$2
+$3
