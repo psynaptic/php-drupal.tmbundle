aaa9d7187bacc2a812804e6e50a6861f191e7792
Balazs Dianiska <balazs@longlake.co.uk>
2010-04-11 23:38:25 +0100

    Updating the other preprocesses as well to d6 syntax

--- Support/commands/preprocess/template_preprocess_page.6.php
+++ Support/commands/preprocess/template_preprocess_page.6.php
@@ -3,10 +3,8 @@
  *
  * @param \$variables
  *   An array of variables to pass to the theme template.
- * @param \$hook
- *   The name of the template being rendered ("page" in this case.)
  */
-function ${1:phptemplate}_preprocess_page(&\$variables, \$hook) {
+function ${1:phptemplate}_preprocess_page(&\$variables) {
   $2
 }
 
