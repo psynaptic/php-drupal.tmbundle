be0c9ccf4696fdd2cf4cad3c250db1e7ee2876c9
Lev Tsypin <lev@levelos.com>
2010-10-28 15:08:17 -0700

    adding hook_form_alter for D7

---
+++ Support/commands/hooks/hook_form_alter.7.php
@@ -0,0 +1,12 @@
+/**
+ * Implements of hook_form_alter().
+ */
+function <?php print $basename; ?>_form_alter(&\$form, &\$form_state, \$form_id) {${1:
+  switch (\$form_id) {
+    case '${2:form_id}':
+      ${3:// Form modification code goes here.}
+      break;
+  \}}
+}
+
+$6
