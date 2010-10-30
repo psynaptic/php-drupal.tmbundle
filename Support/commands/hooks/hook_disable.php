239d5fffa672ab3ba6bd9efed66580a744e2fc38
webflo <florian@webflo.org>
2010-10-30 11:07:27 +0200

    add hook_enable and hook_disable

---
+++ Support/commands/hooks/hook_disable.php
@@ -0,0 +1,8 @@
+/**
+ * Implementation of hook_disable().
+ */
+function ${1:<?php print $basename; ?>}_disable() {
+  ${2:// Code goes here.}
+}
+
+$3
---
+++ Support/commands/hooks/hook_enable.php
@@ -0,0 +1,8 @@
+/**
+ * Implementation of hook_enable().
+ */
+function ${1:<?php print $basename; ?>}_enable() {
+  ${2:// Code goes here.}
+}
+
+$3
