5d05fdaa064b28bf9d939f3d271746a772cb94db
webflo <florian@webflo.org>
2010-10-30 11:05:24 +0200

    add hook_delete for d5, d6 and d7

---
+++ Support/commands/hooks/hook_delete.7.php
@@ -0,0 +1,8 @@
+/**
+ * Implementation of hook_delete().
+ */
+function ${1:<?php print $basename; ?>}_delete(\$node) {
+  ${2:db_query('DELETE FROM \{mytable\} WHERE nid = %d', \$node->nid);}
+}
+
+$3  
