9f6d5d34db7399dcd2b5ea264610d7fe68cf5a25
webflo <florian@webflo.org>
2010-10-30 11:06:03 +0200

    add hook_form for d5, d6, and d7

---
+++ Support/commands/hooks/hook_form.6.php
@@ -0,0 +1,17 @@
+/**
+ * Implementation of hook_form().
+ */
+function ${1:<?php print $basename; ?>}_form(&\$node, \$form_state) {
+  $form = array();
+  $type = node_get_types('type', $node);
+  
+  $form['title'] = array(
+    '#type'=> 'textfield',
+    '#title' => check_plain($type->title_label),
+    '#required' => TRUE,
+  );
+    
+  return $form;
+}
+
+$2
