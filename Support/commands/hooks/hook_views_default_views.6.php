5645ba7428c8b4010e9f3444897a21c5cbe096dd
Balazs Dianiska <balazs@longlake.co.uk>
2010-11-12 12:59:42 +0000

    Better default code for default views.

--- Support/commands/hooks/hook_views_default_views.6.php
+++ Support/commands/hooks/hook_views_default_views.6.php
@@ -2,13 +2,19 @@
  * Implements hook_views_default_views().
  */
 function <?php print $basename; ?>_views_default_views() {
-  \$view = new view;
-  \$view->name = '${1:view_name}';
-  
-  ${2:// Code goes here.}
-  
-  \$views[\$view->name] = \$view;
-  return \$views;
+  ${1:static \$views;
+
+  if (isset(\$views)) {
+    return \$views;
+  }
+  \$files = file_scan_directory(drupal_get_path('module', '<?php print $basename; ?>') . '/views_default', '.inc');
+  foreach (\$files as \$filepath => \$file) {
+    include \$filepath;
+    if (isset($view)) {
+      $views[\$view->name] = \$view;
+    }
+  }
+  return \$views;}
 }
 
-$3
+$2
