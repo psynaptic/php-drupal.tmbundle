94099fd1b6db0b0d6e43d18b410662644987f360
webflo <florian@webflo.org>
2011-01-09 17:23:41 +0100

    fix bugs in snufkins commit. https://github.com/psynaptic/php-drupal.tmbundle/commit/2196fbeac23c8875d5a75f1d7fa63f128c749157
    - hook_block is not supported in D7. Rename hook_block to hook_block.5.php and hook_block.6.php
    - Add new hooks for D7
    - Add menu items for all hooks in this commit.

---
+++ Support/commands/hooks/hook_block.6.php
@@ -0,0 +1,44 @@
+/**
+ * Implements hook_block().
+ */
+function <?php print $basename; ?>_block(\$op = 'list', \$delta = 0, \$edit = array()) {
+  switch (\$op) {
+
+    case 'list':
+      \$blocks[0]['info'] = t('${1:<?php print ucfirst($basename); ?> block 1}');
+      ${2:\$blocks[1]['info'] = t('${3:<?php print ucfirst($basename); ?> block 2}');
+      }return \$blocks;
+
+    ${4:case 'configure':
+      if (\$delta == 0 && user_access('administer module')) {
+        \$form['module_block_1'] = ${5:array()};
+      \}
+      ${6:if (\$delta == 1 && user_access('administer module')) {
+        \$form['module_block_2'] = ${7:array()};
+      \}
+      }return \$form;
+
+    case 'save':
+      if (\$delta == 0) {
+        ${8:variable_set('module_block_setting_1', \$edit['module_block_1']);}
+      \}
+      ${9:if (\$delta == 1) {
+        variable_set('module_block_setting_2', \$edit['module_block_2']);
+      \}
+      }break;
+
+    }case 'view':
+      if (\$delta == 0) {
+        \$block['subject'] = t('${10:<?php print ucfirst($basename); ?> block 1 title}');
+        \$block['content'] = t('${11:<?php print ucfirst($basename); ?> block 1 content}');
+      }
+      ${12:if (\$delta == 1) {
+        \$block['subject'] = t('<?php print ucfirst($basename); ?> block 2 title');
+        \$block['content'] = t('<?php print ucfirst($basename); ?> block 2 content');
+      \}
+}
+      return \$block;
+  }    
+}
+
+$13
---
+++ Support/commands/hooks/hook_block_view_alter.7.php
@@ -0,0 +1,8 @@
+/**
+ * Implements hook_block_view_alter().
+ */
+function <?php print $basename; ?>_block_view_alter(&\$data, \$block) {
+  ${1:// Code goes here.}
+}
+
+$2
