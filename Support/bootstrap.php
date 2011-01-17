f5b48af76f571d57d2c34fd58c1f838aad7ea952
webflo <florian@webflo.org>
2011-01-17 20:12:02 +0100

    API Lookup: textmate_docs_for_word should be compatible to all api-sites. implement a improved lookup for drupalcontrib.org

--- Support/bootstrap.php
+++ Support/bootstrap.php
@@ -219,13 +219,16 @@ function textmate_detect_drupal_theme() {
 function textmate_docs_for_word($function) {
   global $basename;
   global $version;
-  $suggestions = array();
+
+  if (strpos($_ENV['TM_DRUPAL_API'], 'drupalcontrib.org') !== FALSE) {  
+    $suggestions = array();
+    $suggestions[] = preg_replace('/\A' . $basename .'_/', 'hook_', $function, 1);
+    $suggestions[] = preg_replace('/\A' . $basename .'_/', 'theme_', $function, 1);
+    $suggestions[] = $function;
   
-  $suggestions[] = preg_replace('/\A' . $basename .'_/', 'hook_', $function, 1);
-  $suggestions[] = preg_replace('/\A' . $basename .'_/', 'theme_', $function, 1);
-  $suggestions[] = $function;
+    $suggestions = array_unique($suggestions);
+    return $_ENV['TM_DRUPAL_API'] . '/textmate_api/search/' . $version . '/' . implode('/', $suggestions);    
+  }
   
-  $suggestions = array_unique($suggestions);
-  
-  return $_ENV['TM_DRUPAL_API'] . '/textmate_api/search/' . $version . '/' . implode('/', $suggestions);
+  return $_ENV['TM_DRUPAL_API'] . '/api/search/' . $version . '/' . $function;    
 }
