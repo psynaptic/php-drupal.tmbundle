d7b94a4f96f9451292d0919d5a56bac069c06a39
webflo <florian@webflo.org>
2011-01-25 06:44:25 +0800

    fix hook_views_pre_view for drupal 6.

--- Support/commands/custom/hooks/hook_views_pre_view.6.php
+++ Support/commands/custom/hooks/hook_views_pre_view.6.php
@@ -1,7 +1,7 @@
 /**
  * Implements hook_views_pre_view().
  */
-function <?php print $basename; ?>_views_pre_view(&\$view, &\$display_id, &\$args)) {
+function <?php print $basename; ?>_views_pre_view(&\$view, &\$display_id, &\$args) {
   switch (\$view->name) {
     case '${1:view_name}':
       ${2:// View modification code goes here.}
