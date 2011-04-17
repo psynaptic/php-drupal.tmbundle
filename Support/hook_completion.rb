58e1e33236e2619e35725225068ce308175df3dd
webflo <florian@webflo.org>
2011-04-17 11:17:46 +0200

    set Drupal 7 as default version. fix bug with untitled files.

--- Support/hook_completion.rb
+++ Support/hook_completion.rb
@@ -3,11 +3,11 @@ require ENV['TM_SUPPORT_PATH'] + '/lib/ui'
 require ENV['TM_BUNDLE_SUPPORT'] + '/misc/ini.rb'
 require 'find'
 
-path = File.dirname(ENV['TM_FILEPATH']).split('/')
-if !ENV['TM_DRUPAL_VERSION']
-  print 'The PHP Drupal Bundle is not installed properly. Press ⌃H for instructions.'
-else
-  version = ENV['TM_DRUPAL_VERSION']
+version = ENV['TM_DRUPAL_VERSION'] || 7
+version = version.to_s
+
+if !ENV['TM_FILEPATH'].nil?
+  path = File.dirname(ENV['TM_FILEPATH']).split('/')
   while !path.empty?
     folder_info = Dir[path.join('/') + '/*.info', path.join('/') + '/modules/system/system.info']
     if !folder_info.empty?
@@ -19,7 +19,12 @@ else
     end
     path.pop
   end
+end
 
-  choices = OSX::PropertyList.load(File.read(ENV['TM_BUNDLE_SUPPORT'] + '/commands/generated/functions.' + version + '.plist'))
+plist = ENV['TM_BUNDLE_SUPPORT'] + '/commands/generated/functions.' + version + '.plist'
+if File.exists?(plist)
+  choices = OSX::PropertyList.load(File.read(plist))
   TextMate::UI.complete(choices, :initial_filter => ENV['TM_CURRENT_WORD'], :extra_chars => '_')
+else
+  print "Function completion is not available for your Drupal version."
 end
