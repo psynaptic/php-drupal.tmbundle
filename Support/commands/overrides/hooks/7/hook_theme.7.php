cf32da3f482c6ecd09bdd1ea7b6f7f86d00d359e
Florian Weber <florian@webflo.org>
2011-09-29 23:15:18 +0200

    Issue #1294234: Fix hook_theme. rename arguments to variables.

--- Support/commands/overrides/hooks/7/hook_theme.7.php
+++ Support/commands/overrides/hooks/7/hook_theme.7.php
@@ -4,7 +4,7 @@
 function <?php print $basename; ?>_theme(\$existing, \$type, \$theme, \$path) {
   return array(
     '${1:example}' => array(
-      'arguments' => array('${2:arg0}' => NULL, '${3:arg1}' => NULL),
+      'variables' => array('${2:arg0}' => NULL, '${3:arg1}' => NULL),
       'template' => '${4:example}',
     ),
   );
